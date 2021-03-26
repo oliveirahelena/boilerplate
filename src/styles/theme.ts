061e8a004dca674e8dd540f8f210c270c1758f97
Helena Oliveira <helena.oliveira@casaldev.com.br>
2021-03-26 10:21:45 -0300

    add linting

---
+++ src/styles/theme.ts
@@ -0,0 +1,45 @@
+export const theme = {
+  colors: {
+    primaryColor: '#61DAFB',
+    secondaryColor: '#EBA417',
+    background: '#121214',
+    text: '#FFFFFF',
+    white: '#FFFFFF',
+    lightGray: '#E1E1E6',
+    mediumGray: '#A8A8B3',
+    darkGray: '#121214',
+    lightBg: '#F0F2F5',
+    darkBg: '#0A1128'
+  },
+  font: {
+    family: {
+      default: "'Poppins', sans-serif",
+      secondary: "'Montserrat', sans-serif"
+    },
+    sizes: {
+      xsmall: '0.8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem'
+    }
+  },
+  media: {
+    lteMedium: '(max-width: 768px)'
+  },
+  spacings: {
+    xsmall: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem'
+  },
+  borders: {},
+  effects: {}
+} as const
