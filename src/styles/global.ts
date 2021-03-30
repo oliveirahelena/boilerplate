af124de8a3dae3a3e0a8cae967481e0a87d80bbc
Helena Oliveira <helena.oliveira@casaldev.com.br>
2021-03-30 14:43:39 -0300

    change styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,30 +14,39 @@ const GlobalStyles: GlobalStyleComponent<
   DefaultTheme
 > = createGlobalStyle`
   @font-face {
-    font-family: 'Poppins';
+    font-family: 'Montserrat';
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local('Poppins Light'), local('Poppins-Light'),
-        url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
+    src: local('Montserrat Light'), local('Montserrat-Light'),
+        url('/fonts/Montserrat-Light.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: 'Poppins';
+    font-family: 'Montserrat';
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local('Poppins Regular'), local('Poppins-Regular'),
-        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
+    src: local('Montserrat Regular'), local('Montserrat-Regular'),
+        url('/fonts/Montserrat-Regular.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: 'Poppins';
+    font-family: 'Montserrat';
     font-style: normal;
-    font-weight: 600;
+    font-weight: 500;
     font-display: swap;
-    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
-        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
+    src: local('Montserrat Medium'), local('Montserrat-Medium'),
+        url('/fonts/Montserrat-Medium.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local('Montserrat Bold'), local('Montserrat-Bold'),
+        url('/fonts/Montserrat-Bold.woff2') format('woff2');
   }
 
   * {
@@ -54,18 +63,68 @@ const GlobalStyles: GlobalStyleComponent<
   }
 
   ${({ theme, removeBg }) => css`
-    html {
-      font-size: 62.5%;
+    @media ${theme.media.lteLarge} {
+      html {
+        font-size: 93.75%;
+      }
+    }
+
+    @media ${theme.media.lteMedium} {
+      html {
+        font-size: 87.5%;
+      }
     }
 
     body {
-      font-family: ${theme.font.family};
-      font-size: ${theme.font.sizes.medium};
-
       ${!removeBg &&
       css`
-        background-color: ${theme.colors.mainBg};
+        background-color: ${theme.colors.lightBg};
       `}
+      color: ${theme.colors.black};
+    }
+
+    body,
+    input,
+    textarea,
+    select,
+    button {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+      font-weight: ${theme.font.regular};
+    }
+
+    button {
+      cursor: pointer;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    h1 {
+      font-size: ${theme.font.sizes.xhuge};
+      font-weight: ${theme.font.light};
+    }
+
+    h2 {
+      font-size: ${theme.font.sizes.huge};
+      font-weight: ${theme.font.medium};
+    }
+
+    h3 {
+      font-size: ${theme.font.sizes.xxlarge};
+      font-weight: ${theme.font.bold};
+    }
+
+    h4 {
+      font-size: ${theme.font.sizes.xlarge};
+      font-weight: ${theme.font.bold};
+    }
+
+    h5 {
+      font-size: ${theme.font.sizes.large};
+      font-weight: ${theme.font.bold};
     }
   `}
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,47 +1,63 @@
 export default {
   grid: {
-    container: '130rem',
-    gutter: '3.2rem'
+    container: '70rem',
+    gutter: '2.188rem'
+  },
+  media: {
+    lteMedium: '(max-width: 720px)',
+    lteLarge: '(max-width: 1080px)'
   },
   border: {
-    radius: '0.4rem'
+    radius: '0.5rem'
   },
   font: {
     family:
-      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     light: 300,
-    normal: 400,
-    bold: 600,
+    regular: 400,
+    medium: 500,
+    bold: 700,
     sizes: {
-      xsmall: '1.2rem',
-      small: '1.4rem',
-      medium: '1.6rem',
-      large: '1.8rem',
-      xlarge: '2.0rem',
-      xxlarge: '2.8rem',
-      huge: '5.2rem'
+      xxsmall: '0.625rem',
+      xsmall: '0.75rem',
+      small: '0.875rem',
+      medium: '1rem',
+      large: '1.25rem',
+      xlarge: '1.625rem',
+      xxlarge: '2rem',
+      huge: '2.5rem',
+      xhuge: '3.25rem'
     }
   },
   colors: {
-    primary: '#F231A5',
-    secondary: '#3CD3C1',
-    mainBg: '#06092B',
+    primary: '#E65612',
+    secondary: '#33FF7C',
+    tertiary: '#1994FF',
+    darkBg: '#06092B',
     lightBg: '#F2F2F2',
-    white: '#FAFAFA',
-    black: '#030517',
-    lightGray: '#EAEAEA',
-    gray: '#8F8F8F',
-    darkGray: '#2E2F42',
-    red: '#FF6347'
+    white: '#FFFFFF',
+    black: '#000000',
+    lightGray: '#E5E5E5',
+    gray: '#B3B3B3',
+    darkGray: '#808080',
+    red: '#F20505'
   },
   spacings: {
-    xxsmall: '0.8rem',
-    xsmall: '1.6rem',
-    small: '2.4rem',
-    medium: '3.2rem',
-    large: '4.0rem',
-    xlarge: '4.8rem',
-    xxlarge: '5.6rem'
+    xxsmall: '0.5rem',
+    xsmall: '1rem',
+    small: '1.5rem',
+    medium: '2rem',
+    large: '2.5rem',
+    xlarge: '3.5rem',
+    xxlarge: '4.5rem'
+  },
+  shadows: {
+    xxsmall: '0px 1px 2px rgba(0, 0, 0, 0.16)',
+    xsmall: '0px 2px 4px rgba(0, 0, 0, 0.16)',
+    small: '0px 4px 8px rgba(0, 0, 0, 0.16)',
+    medium: '0px 8px 16px rgba(0, 0, 0, 0.16)',
+    large: '0px 12px 24px rgba(0, 0, 0, 0.16)',
+    xlarge: '0px 16px 32px rgba(0, 0, 0, 0.16)'
   },
   layers: {
     base: 10,
